967c1c45521c0652a32a2e619afb2463c031de36
MINTILER-DEV <antonioozurumba@gmail.com>
2026-08-09 09:03:09 -0500

    use msys2 for build

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 @echo off
 echo building
 cd ./src
-g++ -std=c++20 main.cpp uwuifier.cpp -o ../build/uwuifier.exe
+g++ -std=c++20 -static-libgcc -static-libstdc++ main.cpp uwuifier.cpp -o uwuifier.exe
 cd .. 
