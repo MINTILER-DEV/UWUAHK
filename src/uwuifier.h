159bf6616ad35b35c03c387340047f19ce966362
MINTILER-DEV <antonioozurumba@gmail.com>
2026-08-09 22:31:58 -0500

    increased chances

--- src/uwuifier.h
+++ src/uwuifier.h
@@ -6,9 +6,9 @@ namespace uwuifier {
     class Settings {
     public:
         double periodToExclamationChance = 0.2;
-        double stutterChance = 0.1;
-        double presuffixChance = 0.1;
-        double suffixChance = 0.3;
+        double stutterChance = 0.2;
+        double presuffixChance = 0.2;
+        double suffixChance = 0.45;
         double duplicateCharactersChance = 0.4;
         int duplicateCharactersAmount = 3;
     };
